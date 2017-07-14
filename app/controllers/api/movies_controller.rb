8477f1c3ef6b0f0f615ba008df4f472381457d98
Luke Ghenco <lukeghenco@gmail.com>
2017-07-13 21:56:03 -0400

    Add movies#create method

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -3,4 +3,18 @@ class Api::MoviesController < ApplicationController
     @movies = Movie.all 
     render json: @movies
   end
+
+  def create 
+    @movie = Movie.new(movie_params)
+    if @movie.save 
+      render json: @movie 
+    else 
+      render json: { errors: { message: "This Movie Failed To Save" }}
+    end 
+  end
+
+  private 
+    def movie_params 
+      params.require(:movie).permit(:title, :release_year)
+    end
 end
